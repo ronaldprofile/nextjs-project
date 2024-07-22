c8361e9b41c2542207c5948e59f279cec50786c4
Ronald Tomaz <ronaldtmprofile@gmail.com>
2024-07-22 11:31:32 -0300

    add interceptor request

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -3,3 +3,12 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://apicomercial.podiotecnologia.com.br'
 })
+
+api.interceptors.request.use(config => {
+  if (typeof window !== 'undefined') {
+    console.log('interceptor request')
+    console.log(window.location.host)
+  }
+
+  return config
+})
